6f18034ef5733878404b3bb7536f018286b72d5f
rkamalesh07 <rkamalesh@ucsd.edu>
2026-02-22 17:48:10 -0800

    hardcode API URL in next.config.js

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://swingfactr-production.up.railway.app',
+    NEXT_PUBLIC_API_URL: 'https://swingfactr-production.up.railway.app',
   },
 }
+
 module.exports = nextConfig
